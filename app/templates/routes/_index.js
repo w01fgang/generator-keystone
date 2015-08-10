84bb39c37715e03da36de4002b62b9f9187affb5
Denis Sumin <sumin@unix-center.ru>
2015-08-10 16:28:54 +0200

    Added error handler
    Added 404, 505 errors handler in middleware and index routes

--- app/templates/routes/_index.js
+++ app/templates/routes/_index.js
@@ -1,19 +1,19 @@
 <% if (includeGuideComments) { %>/**
  * This file is where you define your application routes and controllers.
- * 
+ *
  * Start by including the middleware you want to run for every request;
  * you can attach middleware to the pre('routes') and pre('render') events.
- * 
+ *
  * For simplicity, the default setup for route controllers is for each to be
  * in its own file, and we import all the files in the /routes/views directory.
- * 
+ *
  * Each of these files is a route controller, and is responsible for all the
  * processing that needs to happen for the route (e.g. loading data, handling
  * form submissions, rendering the view template, etc).
- * 
+ *
  * Bind each route pattern your application should respond to in the function
  * that is exported from this module, following the examples below.
- * 
+ *
  * See the Express application routing documentation for more information:
  * http://expressjs.com/api.html#app.VERB
  */
@@ -23,8 +23,24 @@ var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
 
 // Common Middleware
+keystone.pre('routes', middleware.initErrorHandlers);
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
+
+// Handle 404 errors
+keystone.set('404', function(req, res, next) {
+    res.notfound();
+});
+
+// Handle other errors
+keystone.set('500', function(err, req, res, next) {
+    var title, message;
+    if (err instanceof Error) {
+        message = err.message;
+        err = err.stack;
+    }
+    res.err(err, title, message);
+});
 
 // Import Route Controllers
 var routes = {
@@ -33,7 +49,7 @@ var routes = {
 
 // Setup Route Bindings
 exports = module.exports = function(app) {
-	
+
 	// Views
 	app.get('/', routes.views.index);
 	<% if (includeBlog) { %>app.get('/blog/:category?', routes.views.blog);
